9c39c1e6a8fea06698224663d3ec883dcabb7691
goblincore <donny@Donnys-MacBook-Pro.local>
2020-02-03 13:35:50 -0500

    update dep

--- src/test.js
+++ src/test.js
@@ -23,7 +23,7 @@ yas.listen(port, () => {
 });
 
 // Get metadata.
-yas.get('HQmmM_qwG4k', (err, data) => {
+yas.get('DtVR2M5-A-c', (err, data) => {
   console.log('-'.repeat(80));
   console.log('GOT METADATA for HQmmM_qwG4k:', data || err);
 });
